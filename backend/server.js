87eb7c8721ca91651abd4a4b4787e60c51b6fc39
GabrieleJasiunaite <jasinait.gabriel@gmail.com>
2024-01-08 21:55:51 +0200

    deleted repeating code

--- backend/server.js
+++ backend/server.js
@@ -25,13 +25,6 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['*']);
-    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.append('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
-
 //routes
 app.use('/api/cars', carsRoutes);
 app.use('/api/user', userRoutes);
