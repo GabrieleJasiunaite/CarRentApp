fceedc973cf7c90585fee23d444699d2ab073e46
valsin132 <valdemarassinkevicius@gmail.com>
2024-01-08 16:33:23 +0200

    cars page  ir ziureti btn

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,13 @@ app.use((req, res, next) => {
     next()
 })
 
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 //routes
 app.use('/api/cars', carsRoutes)
 
