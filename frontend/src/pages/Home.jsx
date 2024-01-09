fd98ec76ebac23222edf4a2829ab8d4b59a828da
GabrieleJasiunaite <jasinait.gabriel@gmail.com>
2024-01-09 08:49:07 +0200

    added home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,101 @@
+import CarRentPic from "../pictures/Car-rent-pic.png"
+import CarRentPic2 from "../pictures/Car-rent-pic2.png"
+import Brands from "../pictures/Brands.png"
+import { useEffect, useState } from "react"
+
 const Home = () => {
+    const [carsData, setCarsData] = useState([])
+    const carIds = ['659807eacb3a6fcedfcf501b', "659807eacb3a6fcedfcf5028", "659807eacb3a6fcedfcf501f", "659807eacb3a6fcedfcf5020"]
+
+    useEffect(() => {
+        const fetchCarsByIds = async () => {
+            try {
+                const promises = carIds.map(async id => {
+                    const response = await fetch(`http://localhost:8000/api/cars/${id}`)
+                    if (response.ok) {
+                        const data = await response.json()
+                        return data
+                    }
+                })
+
+                const cars = await Promise.all(promises)
+                setCarsData(cars.filter(Boolean))
+            } catch (error) {
+                console.error('Error fetching data:', error)
+            }
+        }
+
+        fetchCarsByIds()
+    }, [])
+
     return (
         <>
+            <div className="first-container">
+                <div className="first-container-text">
+                    <h1><strong>Netikėtai prireikė išsinuomoti automobilį? <br />
+                        Čia jį rasite už <span>ypač mažą kainą.</span></strong></h1>
+                    <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. </p>
+                </div>
+                <img src={CarRentPic} alt="car-rent-logo" />
+            </div>
+            <div className="second-container">
+                <h2>Populiariausi automobiliai</h2>
+                <div className="cars-display-container">
+                    {carsData.map(car => (
+                        <div className="car-display-container" key={car._id}>
+                            <div className="car-pic-box">
+                                <img src={car.imageUrl} alt={`${car.brand} ${car.model}`} />
+                                <h3>{car.brand} {car.model}, {car.year}</h3>
+                            </div>
+                            <div className="car-info-box">
+                                <p><span className="iconify" data-icon="f7:car-fill"></span> {car.body}</p>
+                                <p><span className="iconify" data-icon="game-icons:car-seat"></span> {car.seats} vietų</p>
+                                <p><span className="iconify" data-icon="bi:fuel-pump"></span>{car.fuelType} </p>
+                                <p><span className="iconify" data-icon="game-icons:gear-stick-pattern"></span> {car.transmission}</p>
+                            </div>
+                            <div className="car-button-box">
+                                <p>{car.price} Eur <span>/parai</span></p>
+                                <button>Daugiau <i class="bi bi-arrow-right"></i></button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className="third-container">
+                <img src={CarRentPic2} className="third-pic" alt="car pick" />
+                <div className="num1 block">
+                    <img src="../images/1.png" alt="number 1" />
+                    <h3>Prisiregistruokite</h3>
+                    <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
+                </div>
+                <div className="num2 block">
+                    <img src="../images/2.png" alt="number 2" />
+                    <h3>Pasirinkite automobilį</h3>
+                    <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
+                </div>
+                <div className="num3 block">
+                    <img src="../images/3.png" alt="number 3" />
+                    <h3>Pasirinkite nuomos datą</h3>
+                    <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
+                </div>
+                <div className="num4 block">
+                    <img src="../images/4.png" alt="number 4" />
+                    <h3>Patvirtinkite savo užsakymą</h3>
+                    <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
+                </div>
+                <div className="num5 block">
+                    <img src="../images/5.png" alt="number 5" />
+                    <h3>Laukite patvirtinimo</h3>
+                    <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
+                </div>
+            </div>
+
+            <div className="fourth-container">
+                <img src={Brands} alt="brands logos" />
+            </div>
+
         </>
-    );
-};
+    )
+}
 
 export default Home;
